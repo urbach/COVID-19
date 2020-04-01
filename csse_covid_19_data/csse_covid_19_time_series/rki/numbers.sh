ba715331b64d65a9296f3090dee9643ff334de8a
Carsten Urbach <curbach@gmx.de>
2020-04-01 22:19:18 +0200

    ...

--- csse_covid_19_data/csse_covid_19_time_series/rki/numbers.sh
+++ csse_covid_19_data/csse_covid_19_time_series/rki/numbers.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pdfgrep  "Nordrhein-Westfalen   " 2020-0* | grep -v National | grep -v Regionen | grep -v Tirol | tr -s '[:blank:]"+"*[:blank:]' ',' | sed -e {s/-de.pdf?__blob=publicationFile://g} | sed -e {s/','//g} | sed -e {s/Landkreis//g} | sed -e {s/Heinsberg//g} > tmp.dat
+pdfgrep  "Nordrhein-Westfalen   " 2020-0* | grep -v National | grep -v Regionen | grep -v Tirol | tr -s '[:blank:]"+"*[:blank:]' ',' | sed -e {s/-de.pdf?__blob=publicationFile://g} | sed -e {s/','//g} | sed -e {s/Landkreis//g} | sed -e {s/Heinsberg//g} | sed -e {s/\\.//g} > tmp.dat
 ## fill missing elements
 awk -F, '{$7=""}1' OFS=',' tmp.dat > nrw.csv
 
-pdfgrep "Gesamt     " 2020-0*  | tr -s '[:blank:]"+"*[:blank:]' ',' | sed -e {s/-de.pdf?__blob=publicationFile://g} > tmp.dat
+pdfgrep "Gesamt     " 2020-0*  | tr -s '[:blank:]"+"*[:blank:]' ',' | sed -e {s/-de.pdf?__blob=publicationFile://g}  | sed -e {s/\\.//g} > tmp.dat
 awk -F, '{$7=""}1' OFS=',' tmp.dat > gesamt.csv
 
 rm tmp.dat
