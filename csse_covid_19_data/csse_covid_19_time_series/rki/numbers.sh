28d18a332cc02988960a4c72c62c6e607e4f820c
Carsten Urbach <curbach@gmx.de>
2020-04-03 09:56:48 +0200

    ...

--- csse_covid_19_data/csse_covid_19_time_series/rki/numbers.sh
+++ csse_covid_19_data/csse_covid_19_time_series/rki/numbers.sh
@@ -4,7 +4,7 @@ pdfgrep  "Nordrhein-Westfalen   " 2020-0* | grep -v National | grep -v Regionen 
 ## fill missing elements
 awk -F, '{$7=""}1' OFS=',' tmp.dat > nrw.csv
 
-pdfgrep "Gesamt     " 2020-0*  | tr -s '[:blank:]"+"*[:blank:]' ',' | sed -e {s/-de.pdf?__blob=publicationFile://g}  | sed -e {s/\\.//g} > tmp.dat
+pdfgrep "Gesamt     " 2020-0*  | grep -v "%" | tr -s '[:blank:]"+"*[:blank:]' ',' | sed -e {s/-de.pdf?__blob=publicationFile://g}  | sed -e {s/\\.//g} > tmp.dat
 awk -F, '{$7=""}1' OFS=',' tmp.dat > gesamt.csv
 
 rm tmp.dat
