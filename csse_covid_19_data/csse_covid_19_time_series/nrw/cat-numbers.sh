f85b4b1e802711d4fef360c561b3448ed3ed8d24
Carsten Urbach <curbach@gmx.de>
2020-08-03 13:04:00 +0200

    update

--- csse_covid_19_data/csse_covid_19_time_series/nrw/cat-numbers.sh
+++ csse_covid_19_data/csse_covid_19_time_series/nrw/cat-numbers.sh
@@ -4,7 +4,7 @@ rm bonn.csv
 
 for f in coronavi*; do
     echo -n $f | sed -e {s/coronavirus-fallzahlen-nrw//g}  >> bonn.csv
-    echo  `grep -A 1 "<td>Bonn</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/\\.//g}'` >> bonn.csv
+    echo  `grep -A 1 "<td>Bonn</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/\\.//g}'` | sed -e '{s/<td class="rtecenter">/,/g}' >> bonn.csv
     ##grep -A 2 "<td>Bonn</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' >> bonn.csv
 done
 
@@ -12,7 +12,7 @@ rm koeln.csv
 
 for f in coronavi*; do
     echo -n $f | sed -e {s/coronavirus-fallzahlen-nrw//g}  >> koeln.csv
-    echo  `grep -A 1 "<td>Köln</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/\\.//g}'` >> koeln.csv
+    echo  `grep -A 1 "<td>Köln</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/\\.//g}'  | sed -e '{s/<td class="rtecenter">/,/g}'` >> koeln.csv
     ##grep -A 2 "<td>Bonn</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' >> koeln.csv
 done
 
@@ -20,7 +20,7 @@ rm duesseldorf.csv
 
 for f in coronavi*; do
     echo -n $f | sed -e {s/coronavirus-fallzahlen-nrw//g}  >> duesseldorf.csv
-    echo  `grep -A 1 "<td>Düsseldorf</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/\\.//g}'` >> duesseldorf.csv
+    echo  `grep -A 1 "<td>Düsseldorf</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/\\.//g}' | sed -e '{s/<td class="rtecenter">/,/g}'` >> duesseldorf.csv
     ##grep -A 2 "<td>Bonn</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' >> duesseldorf.csv
 done
 
@@ -28,7 +28,7 @@ rm heinsberg.csv
 
 for f in coronavi*; do
     echo -n $f | sed -e {s/coronavirus-fallzahlen-nrw//g}  >> heinsberg.csv
-    echo  `grep -A 1 "<td>Heinsberg (Kreis)</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/\\.//g}'` >> heinsberg.csv
+    echo  `grep -A 1 "<td>Heinsberg (Kreis)</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/\\.//g}' | sed -e '{s/<td class="rtecenter">/,/g}'` >> heinsberg.csv
     ##grep -A 2 "<td>Bonn</td>" $f | tail -1 | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' >> heinsberg.csv
 done
 
@@ -37,5 +37,6 @@ rm nrw.csv
 
 for f in coronavi*; do
     echo -n $f | sed -e {s/coronavirus-fallzahlen-nrw//g}  >> nrw.csv
-    echo `grep -A 1 "<strong>Gesamt</strong>" $f | tail -1 | sed -e '{s/<td><strong>/,/g}' | sed -e '{s/<\/strong><\/td>//g}' | sed -e '{s/\\.//g}' | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}'` >> nrw.csv
+    echo -n `grep -A 1 "<strong>Gesamt</strong>" $f | tail -1 | sed -e '{s/<td><strong>/,/g}' | sed -e '{s/<\/strong><\/td>//g}' | sed -e '{s/\\.//g}' | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/<td class="rtecenter"><strong>/,/g}'` >> nrw.csv
+    echo `grep -A 1 "<strong>Nordrhein-Westfalen </strong>" $f | tail -1 | sed -e '{s/<td><strong>/,/g}' | sed -e '{s/<\/strong><\/td>//g}' | sed -e '{s/\\.//g}' | sed -e '{s/<td>/,/g}' | sed -e '{s/<\/td>//g}' | sed -e '{s/<td class="rtecenter"><strong>/,/g}' | sed -e '{s/<td class="rtecenter">/,/g}'` >> nrw.csv
 done
